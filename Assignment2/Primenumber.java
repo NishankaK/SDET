54b9490982f7b356b0d8b05884d5a7c304742d95
NishankaK <91666360+NishankaK@users.noreply.github.com>
2021-09-30 17:19:18 +0530

    Programs Uploaded

---
+++ Assignment2/Primenumber.java
@@ -0,0 +1,27 @@
+package Assignment2;
+
+public class Primenumber {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=13;{
+			boolean isprime=true;
+			for(int i=2;i<num/2;i++) {
+				if(num%i==0) {
+					isprime=false;
+					break;
+
+				}
+			}
+			if(isprime) {
+				System.out.println(num+"is a prime number");
+			}
+			else {
+				System.out.println(num+"is not a prime number");
+			}
+		}
+
+
+	}
+
+}
