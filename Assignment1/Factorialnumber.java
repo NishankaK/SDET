6a9f4a0b2d36b1820a537c66097e7fd3d66841a9
NishankaK <91666360+NishankaK@users.noreply.github.com>
2021-09-30 15:51:09 +0530

    Program uploaded

---
+++ Assignment1/Factorialnumber.java
@@ -0,0 +1,18 @@
+package Assignment1;
+
+public class Factorialnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = 10;
+		long fact = 1;
+		int i = 1;
+		while (i < number) {
+			fact = fact * i;
+			i++;
+
+		}
+		System.out.println("Factorial of " + number + " is:" + fact);
+	}
+}
